2d0959edf242855026c1b7d29781cd18655c808e
Peyton Bock <bockps@mail.uc.edu>
2025-04-24 13:29:33 -0400

    Added my part and ran good.

--- SheriffFinley_FinalProject/mainPackage/main.py
+++ SheriffFinley_FinalProject/mainPackage/main.py
@@ -1,6 +1,6 @@
 # File Name : main.py
-# Student Name: Peter Phan,
-# email:  phanpv@mail.uc.edu, 
+# Student Name: Peter Phan, Peyton Bock
+# email:  phanpv@mail.uc.edu, bockps@mail.uc.edu
 # Assignment Number: Assignment Final Project
 # Due Date:   5/01/2025
 # Course #/Section:   IS4010-001
@@ -9,9 +9,11 @@
 #                                       and movie quote.Then display what was decrypted and a photo of the location
 # Brief Description of what this module does. Develop programs based on problem descriptions and pseudo-code
 # Citations: Gemini: https://gemini.google.com/app
+# Chatgpt: chat.com
 # Anything else that's relevant:
 
 from locationPackage.location import LocationDecrypter
+from moviePackage.movie import MovieDecrypter
 
 if __name__ == "__main__":
     encrypted_location_file = "EncryptedGroupHints Spring 2025.json"
@@ -24,3 +26,11 @@ if __name__ == "__main__":
         print(f"Decrypted Location: {decrypted_location}")
     else:
         print("Failed to decrypt the location.")
+    # --- Step 2: Decrypt the movie title ---
+    movie_key = "gavGFv10Swqor9DHsLKPBkxN14rXEWj1PigAC8Dor-8="
+    movie_file = "data/TeamsAndEncryptedMessagesForDistribution.json"
+    team_name = "Sheriff Finley"
+
+    movie_decrypter = MovieDecrypter(movie_key)
+    decrypted_movie = movie_decrypter.decrypt_movie_title(movie_file, team_name)
+    print(f"Decrypted Movie Title: {decrypted_movie}")
